1a00f2b1ef0e5004ef772f2af72ee9df56160c90
miyagawa <miyagawa@debian.(none)>
2017-08-23 15:19:13 +0900

    To make sys_putc uses hypercall of jailhouse.

--- config/ia32/jailhouse/sys_config.c
+++ config/ia32/jailhouse/sys_config.c
@@ -210,21 +210,41 @@ ER irc_ena_irq(UB irq)
 	return E_OK;
 }
 
-#if SYS_PUTC_PORTID >= 1
-#define SYS_PUT_CHAR(c) pcat_com_putc(SYS_PUTC_PORTID, (c))
-#else
-#define SYS_PUT_CHAR(c) video_putc(c)
-#endif
+static BOOL jailhouse_use_vmcall = TRUE;
+#define JAILHOUSE_HC_DEBUG_CONSOLE_PUTC	8
+#define JAILHOUSE_CALL_CODE	\
+	"cmpb $0x01, %[use_vmcall]\n\t"\
+	"jne 1f\n\t"\
+	"vmcall\n\t"\
+	"jmp 2f\n\t"\
+	"1: vmmcall\n\t"\
+	"2:"
+#define JAILHOUSE_CALL_RESULT	"=a" (result)
+#define JAILHOUSE_USE_VMCALL	[use_vmcall] "m" (jailhouse_use_vmcall)
+#define JAILHOUSE_CALL_NUM	"a" (num)
+#define JAILHOUSE_CALL_ARG1	"D" (arg1)
 
-void
-sys_putc(char c)
+static UW jailhouse_call_arg1(UW num, UW arg1)
 {
-    if (c == '\n') {
-        SYS_PUT_CHAR('\r');
-    }
-    SYS_PUT_CHAR(c);
+	UW result;
+
+	asm volatile(JAILHOUSE_CALL_CODE
+		: JAILHOUSE_CALL_RESULT
+		: JAILHOUSE_USE_VMCALL,
+		  JAILHOUSE_CALL_NUM, JAILHOUSE_CALL_ARG1
+		: "memory");
+	return result;
 }
 
+static void jailhouse_putc(char c)
+{
+	jailhouse_call_arg1(JAILHOUSE_HC_DEBUG_CONSOLE_PUTC, c);
+}
+
+void sys_putc(char c)
+{
+    jailhouse_putc(c);
+}
 
 void
 define_inh(INHNO inhno, FP inthdr)
